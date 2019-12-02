fc73d78abb9238c4205d9624f18a0fc1b162e22e
chenjiacheng <chenjiacheng@soundbus.cn>
2019-12-02 11:21:40 +0800

    Initial commit

---
+++ src/main/java/com/example/hello/controller/TController.java
@@ -0,0 +1,13 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TController {
+
+    @RequestMapping("hello")
+    public String hello(String pa){
+        return "return::" + pa;
+    }
+}
